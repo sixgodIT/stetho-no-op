7a87b5c46dba250737ad80e84e93e9c5e113fc5d
xuzhenzhou <xuzhenzhou@iyunxiao.com>
2017-09-07 21:03:29 +0800

    init project

---
+++ app/src/main/java/com/facebook/stetho/okhttp3/StethoInterceptor.java
@@ -0,0 +1,14 @@
+
+package com.facebook.stetho.okhttp3;
+
+import java.io.IOException;
+
+import okhttp3.Interceptor;
+import okhttp3.Response;
+
+public class StethoInterceptor implements Interceptor {
+    @Override
+    public Response intercept(Chain chain) throws IOException {
+        return chain.proceed(chain.request());
+    }
+}
